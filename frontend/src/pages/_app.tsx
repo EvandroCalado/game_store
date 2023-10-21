5f4eb8ddbbf19472053fb0a3a33b72c2dcb11de6
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-10-21 17:54:52 -0300

    :style: add globals styles

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import 'semantic-ui-css/semantic.min.css';
-import '../styles/globals.css';
+import '../scss/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
