48707f930951f68073b595eb8d56d33357e734fe
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-10-22 23:54:57 -0300

    :chore: add auth provider

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from 'next/app';
 import 'semantic-ui-css/semantic.min.css';
+import { AuthProvider } from '../contexts';
 import '../scss/globals.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
