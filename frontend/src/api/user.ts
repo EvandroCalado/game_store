f88dd358ac6ce1518cf15d4fb29d5bcad8d41fd7
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-10-25 10:04:16 -0300

    :chore: create updame fetch

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -11,4 +11,24 @@ export class User {
 
     return result;
   }
+
+  async updateMe(userId: number, data: object) {
+    const url = `${ENV.API_URL}/${ENV.ENDPOINTS.USERS.UPDATE}/${userId}`;
+
+    const params = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    };
+
+    const response = await authFetch(url, params);
+
+    const result = await (response as Response).json();
+
+    if ((response as Response).status !== 200) throw result;
+
+    return result;
+  }
 }
