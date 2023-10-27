a6baebc273dfd8a3c4b19296b4ccb6f36c132ac4
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-10-26 22:49:57 -0300

    :chore: add get by slug fetch

--- frontend/src/api/platform.ts
+++ frontend/src/api/platform.ts
@@ -14,4 +14,18 @@ export class Platform {
 
     return result;
   }
+
+  async getBySlug(slug: string) {
+    const filters = `filters[slug][$eq]=${slug}`;
+    const populate = 'populate=*';
+    const url = `${ENV.API_URL}/${ENV.ENDPOINTS.PLATFORM}?${filters}&${populate}`;
+
+    const response = await fetch(url);
+
+    const result = await response.json();
+
+    if (response.status !== 200) throw result;
+
+    return result;
+  }
 }
